7491e84172e9cefbc7d75c11bd392a98447e6e9d
amitprabhuglobant <amit.prabhu@globant.com>
2017-04-20 16:27:00 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace nunitsample
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
